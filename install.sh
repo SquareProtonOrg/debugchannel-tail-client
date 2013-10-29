5be462e5ea5a96f8a568f2979cb763c700ea9670
James Tuthill <james@omlet.co.uk>
2013-10-29 23:10:31 +0000

    Added service script to daemonise dctail Added if statement to .install to stop overwriting configuration

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p /etc/debugchannel/
 
-cp "${dir}/src/dctail.conf" /etc/debugchannel/dctail.conf
-cp "${dir}/src/dctail.conf.sample" /etc/debugchannel/dctail.conf.sample
+if [ ! -f /etc/debugchannel/dctail.conf ]; then
+    cp "${dir}/src/etc-debugchannel-dctail.conf" /etc/debugchannel/dctail.conf
+fi
+
+
+cp "${dir}/src/etc-debugchannel-dctail.conf.sample" /etc/debugchannel/dctail.conf.sample
 cp "${dir}/src/dctail" /usr/local/bin/
+chmod 755 /usr/local/bin/dctail
+
+cp "${dir}/src/etc-init.d-dctail" /etc/init.d/dctail
+chmod 755 /etc/init.d/dctail
